6afb7b5277c470a0c3ab9b896e9f2b82caafb34b
정재현 <chwogus0303@naver.com>
2024-02-21 15:16:16 +0900

    init add : dependencies add : application.properties

---
+++ src/test/java/com/study/todoparty/TodoPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.todoparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoPartyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
