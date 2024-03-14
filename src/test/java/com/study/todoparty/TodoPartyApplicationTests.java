d05faa388c876ad793ab124d35e94bd08b682fa7
정재현 <chwogus0303@naver.com>
2024-03-14 23:59:19 +0900

    fix: 테스트코드

---
+++ src/test/java/com/study/todoparty/TodoPartyApplicationTests.java
@@ -0,0 +1,14 @@
+package com.study.todoparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoPartyApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("성공");
+    }
+
+}
